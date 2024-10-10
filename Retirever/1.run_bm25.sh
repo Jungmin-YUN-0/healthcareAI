4d2a76d200c966a60a03550e056c5eb96c41f479
jinhee <78654687+zzhenxi@users.noreply.github.com>
2024-10-11 00:03:00 +0900

    Update Retriever

--- Retirever/1.run_bm25.sh
+++ Retirever/1.run_bm25.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CORPUS_PATH="/home/jinhee/IIPL_PROJECT/rapa/RAG/dataset/msd_kdca_corpus.jsonl"
-QUERY_PATH="/home/jinhee/IIPL_PROJECT/rapa/RAG/dataset/msd_kdca_queries.jsonl"
+CORPUS_PATH="/path/to/dataset/msd_kdca_corpus.jsonl"
+QUERY_PATH="/path/to/dataset/msd_kdca_queries.jsonl"
 K=2
 
 python run_retrieval.py bm25 --corpus_path $CORPUS_PATH \
