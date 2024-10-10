aee8bc7b68a1dca576d381b2a6c2aa6c47e70fe8
jinhee <78654687+zzhenxi@users.noreply.github.com>
2024-10-11 00:03:51 +0900

    Update Retriever

--- Retirever/2.run_dpr.sh
+++ Retirever/2.run_dpr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CORPUS_PATH="/home/jinhee/IIPL_PROJECT/rapa/RAG/dataset/msd_kdca_corpus.jsonl"
-QUERY_PATH="/home/jinhee/IIPL_PROJECT/rapa/RAG/dataset/msd_kdca_queries.jsonl"
+CORPUS_PATH="/path/to/dataset/msd_kdca_corpus.jsonl"
+QUERY_PATH="/path/to/dataset/msd_kdca_queries.jsonl"
 K=2
 MODEL_PATH="nlpai-lab/KoE5"  # embedding model path (DPR) 
 MAX_LENGTH=512  # embedding max_length (DPR)
-EMBEDDING_PATH="/home/jinhee/IIPL_PROJECT/rapa/retrieval/dpr_embeddings/embedding.KoE5.512.pt"
+EMBEDDING_PATH="/path/to/dpr_embeddings/embedding.KoE5.512.pt"
 
 python run_retrieval.py dpr --corpus_path $CORPUS_PATH \
                             --query_path $QUERY_PATH \
