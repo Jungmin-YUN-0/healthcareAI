31995a9500f5451bbd889825db280a7ec73842cb
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2025-06-12 16:38:26 +0900

    Update dialogue_eval_sc.sh

--- MedicalFineTuning/Inference/dialogue_eval_sc.sh
+++ MedicalFineTuning/Inference/dialogue_eval_sc.sh
@@ -3,14 +3,10 @@
 DATA_PATH="/home/jungmin/workspace/project/rapa/data/dialogue_koMedicalChat_preprocessed"
 RESULT_PATH="/home/jungmin/workspace/project/rapa/result/dialogue"
 
-
-
 MODEL_NAME="yanolja/EEVE-Korean-Instruct-10.8B-v1.0"
+CACHE_DIR="/home/project/rapa/final_model/eeve_chatvector_OB_update/1/lora_merged"
 
-CACHE_DIR="/home/jungmin/workspace/project/rapa/healthcareAI/ChatVector/ckpt/eeve_inst_chatvector_OB"
-
-
-CUDA_VISIBLE_DEVICES=3 python dialogue_eval_sc.py \
+python dialogue_eval_sc.py \
     --data_path "$DATA_PATH" \
     --model_name "$MODEL_NAME" \
     --result_path "$RESULT_PATH" \
