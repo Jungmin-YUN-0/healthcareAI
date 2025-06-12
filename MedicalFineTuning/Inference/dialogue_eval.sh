dad64faa39e69aadf2ac3525fd16c3f38542c385
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2025-06-12 16:38:55 +0900

    Update dialogue_eval.sh

--- MedicalFineTuning/Inference/dialogue_eval.sh
+++ MedicalFineTuning/Inference/dialogue_eval.sh
@@ -3,7 +3,6 @@
 DATA_PATH="/home/jungmin/workspace/project/rapa/data/dialogue_koMedicalChat_preprocessed"
 RESULT_PATH="/home/jungmin/workspace/project/rapa/result/dialogue"
 
-
 MODEL_NAME="yanolja/EEVE-Korean-Instruct-10.8B-v1.0"
 CACHE_DIR="/home/project/rapa/final_model/eeve_chatvector_OB_update/1/lora_merged"
 
