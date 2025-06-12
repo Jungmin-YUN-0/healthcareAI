f0aa543cbe2be93f48faf47574341fb11a067adc
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2025-06-12 16:23:46 +0900

    Update add_KO+OB.sh

--- ChatVector/add_KO+OB.sh
+++ ChatVector/add_KO+OB.sh
@@ -1,6 +1,8 @@
 CP_MODEL_PATH=beomi/Llama-3-KoEn-8B
 CHAT_VECTOR_PATH=ckpt_tv/Llama3-OpenBioLLM-8B-chat-vector
 OUTPUT_PATH=ckpt/Llama-3-8B-KoEn-Medical
+CACHE_DIR=[cache directory]
 
 python add_chat_vector.py $CP_MODEL_PATH "['$CHAT_VECTOR_PATH']" $OUTPUT_PATH \
+--cache_dir "$CACHE_DIR" \
 --ratio "[1]"  # chat vector ratio
